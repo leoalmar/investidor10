b31fdaab3774cc1d755492d98ef4125db6a51a26
Leonardo <leonardoxsystems@gmail.com>
2023-04-13 12:22:15 -0300

    Str limit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,8 +11,12 @@
                 <div class="col">
                     <div class="card news-card">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $news->title }}</h5>
-                            <p class="card-text">{{ $news->body }}</p>
+                            <h5 class="card-title">
+                                {{ Str::limit($news->title, 30) }}
+                            </h5>
+                            <p class="card-text">
+                                {{ Str::limit($news->body, 240) }}
+                            </p>
                             <a href="{{ route('news-show', ['newsId' => $news->id], false) }}" class="btn btn-primary">Access</a>
                         </div>
                     </div>
